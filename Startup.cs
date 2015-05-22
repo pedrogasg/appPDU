fd8ef97f85bfa8b9526b80ae134bba0ecb8f8950
unknown <pfillastre@Desktop-034.office.pretdunion.fr>
2015-05-22 16:09:32 +0200

    First Commit

---
+++ Startup.cs
@@ -0,0 +1,22 @@
+
+using Microsoft.AspNet.Builder;
+
+using Microsoft.Framework.DependencyInjection;
+
+namespace appPDU
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+
+        }
+    }
+}
